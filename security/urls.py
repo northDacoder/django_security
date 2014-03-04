56b888bf5a7fc479ed835ad46f7cd657810ed50f
John Ryan <jcryan85@gmail.com>
2014-03-04 13:51:10 -0800

    Added Django-Registration library & dependecies

--- security/urls.py
+++ security/urls.py
@@ -4,13 +4,15 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^secret/$', 'securityapp.views.special_page', name='secret'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+
     # Examples:
     # url(r'^$', 'security.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^signup/$', 'securityapp.views.signup', name='signup'),
-    url(r'^secret/$', 'securityapp.views.special_page', name='secret'),
-    url(r'^accounts/login/$', 'securityapp.views.login_page', name='login'),
+    # url(r'^signup/$', 'securityapp.views.signup', name='signup'),
+    # url(r'^accounts/login/$', 'securityapp.views.login_page', name='login'),
 
 )
